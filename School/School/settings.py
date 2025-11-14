c4ef5fa5ae22310d02549d113870b01d8faf90fb
Yash Pathak <ayushpathak7776@gmail.com>
2025-11-14 17:12:05 +0530

    ALLOWED_HOSTS = ['*'] Added

--- School/School/settings.py
+++ School/School/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-0#36hfyn*k!gjht7-&0pae224$=+o3@=#km6sb)+swz1us%r^a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
